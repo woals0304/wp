5f5168beab186721c89bb242a79bb8d84726885f
VenyVince <redguy0814@gmail.com>
2023-11-08 23:23:20 +0900

    test

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.검색 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.가게이름 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.전화번호 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.주소 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.종류 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.메모 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.되돌리 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "가게 이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 166);
+            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "전화번호";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 230);
+            this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "주소";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 286);
+            this.label4.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "종류";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 356);
+            this.label5.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 27);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "메모";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 634);
+            this.label6.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 27);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "이름 검색";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(73, 734);
+            this.label7.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 27);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "음식 종류";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(240, 83);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 39);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(240, 148);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(324, 39);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(240, 212);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(324, 39);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(240, 268);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(324, 39);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(240, 349);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(324, 213);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(240, 616);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(324, 39);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(240, 716);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(324, 39);
+            this.textBox7.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 83);
+            this.button1.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 40);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "정보저장";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(923, 83);
+            this.button2.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 40);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "정보변경";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1166, 81);
+            this.button3.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 40);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "정보삭제";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // 검색
+            // 
+            this.검색.Location = new System.Drawing.Point(1374, 81);
+            this.검색.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.검색.Name = "검색";
+            this.검색.Size = new System.Drawing.Size(151, 40);
+            this.검색.TabIndex = 17;
+            this.검색.Text = "검 색";
+            this.검색.UseVisualStyleBackColor = true;
+            this.검색.Click += new System.EventHandler(this.검색_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Window;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.가게이름,
+            this.전화번호,
+            this.주소,
+            this.종류,
+            this.메모});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(697, 148);
+            this.listView1.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeftLayout = true;
+            this.listView1.Size = new System.Drawing.Size(822, 607);
+            this.listView1.TabIndex = 19;
+            this.listView1.TileSize = new System.Drawing.Size(300, 30);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // 가게이름
+            // 
+            this.가게이름.Text = "가게이름";
+            this.가게이름.Width = 82;
+            // 
+            // 전화번호
+            // 
+            this.전화번호.Text = "전화번호";
+            this.전화번호.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.전화번호.Width = 82;
+            // 
+            // 주소
+            // 
+            this.주소.Text = "주소";
+            this.주소.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.주소.Width = 82;
+            // 
+            // 종류
+            // 
+            this.종류.Text = "종류";
+            this.종류.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.종류.Width = 82;
+            // 
+            // 메모
+            // 
+            this.메모.Text = "메모";
+            this.메모.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.메모.Width = 82;
+            // 
+            // 되돌리
+            // 
+            this.되돌리.Location = new System.Drawing.Point(1572, 79);
+            this.되돌리.Name = "되돌리";
+            this.되돌리.Size = new System.Drawing.Size(145, 40);
+            this.되돌리.TabIndex = 20;
+            this.되돌리.Text = "되돌리기";
+            this.되돌리.UseVisualStyleBackColor = true;
+            this.되돌리.Click += new System.EventHandler(this.되돌리_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1758, 810);
+            this.Controls.Add(this.되돌리);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.검색);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(7, 4, 7, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button 검색;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader 가게이름;
+        private System.Windows.Forms.ColumnHeader 전화번호;
+        private System.Windows.Forms.ColumnHeader 주소;
+        private System.Windows.Forms.ColumnHeader 종류;
+        private System.Windows.Forms.ColumnHeader 메모;
+        private System.Windows.Forms.Button 되돌리;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 정보저장 버튼 클릭시 발생하는 이벤트.
+        {
+            // 가게 이름, 전화번호, 주소, 음식 종류의 텍스트 박스 확인.
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                // 가게 이름, 전화번호, 주소, 음식 종류, 메모 텍스트 박스 정보를 받음.
+                ListViewItem item = new ListViewItem(textBox1.Text);
+                item.SubItems.Add(textBox2.Text);
+                item.SubItems.Add(textBox3.Text);
+                item.SubItems.Add(textBox4.Text);
+                item.SubItems.Add(textBox5.Text);
+
+                // 가게 이름, 전화번호, 주소, 음식 종류, 메모를 리스트뷰에 추가.
+                listView1.Items.Add(item);
+
+                // 가게 이름, 전화번호, 주소, 음식 종류 텍스트박스 초기화.
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+            }
+            // 가게 이름, 전화번호, 주소, 음식 종류중 미입력 정보가 있으면 메세지박스 띄움.
+            // + 추가할만한 기능 = 메세지박스 확인후 키보드 포커스 설정
+            else if (textBox1.Text == "")
+            {
+                MessageBox.Show("가게 이름을 입력해 주세요.");
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("전화번호를 입력해 주세요.");
+            }
+            else if (textBox3.Text == "")
+            {
+                MessageBox.Show("주소를 입력해 주세요.");
+            }
+            else if (textBox4.Text == "")
+            {
+                MessageBox.Show("음식 종류를 입력해 주세요.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nameToSearch = textBox1.Text; // 검색할 이름
+            string newName = textBox2.Text; // 새로운 이름
+            string newPhone = textBox3.Text; // 새로운 전화번호
+            string newAddress = textBox4.Text; // 새로운 주소
+            string newType = textBox5.Text; // 새로운 종류
+
+
+            // 예시: ListView에서 검색한 이름과 일치하는 항목을 찾고 수정
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.SubItems[0].Text == nameToSearch)
+                {
+                    item.SubItems[0].Text = newName;
+                    item.SubItems[1].Text = newPhone;
+                    item.SubItems[2].Text = newAddress;
+                    item.SubItems[3].Text = newType;
+                }
+            }
+
+
+            // 수정이 성공하면 사용자에게 메시지를 표시합니다.
+            MessageBox.Show("정보가 변경되었습니다.");
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // ListView에서 선택한 항목의 정보를 가져와서 다른 텍스트 상자에 표시
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                textBox1.Text = selectedItem.SubItems[0].Text; // 가게 이름
+                textBox2.Text = selectedItem.SubItems[1].Text; // 전화번호
+                textBox3.Text = selectedItem.SubItems[2].Text; // 주소
+                textBox4.Text = selectedItem.SubItems[3].Text; // 음식 종류
+                textBox5.Text = selectedItem.SubItems[4].Text; // 메모
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("선택하신 항목이 삭제됩니다.\r계속 하시겠습니다?", "항목 삭제", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    int index = listView1.FocusedItem.Index;
+
+                    listView1.Items.RemoveAt(index);
+
+                    MessageBox.Show("삭제되었습니다.");
+                }
+                else
+                {
+                    MessageBox.Show("선택된 항목이 없습니다.");
+                }
+            }
+        }
+
+        // 삭제한 테이터가 들어간 동적배열 리스트
+        private List<ListViewItem> deletedItems = new List<ListViewItem>();
+
+
+        // 이름검색기능
+        private void 검색_Click(object sender, EventArgs e)
+        {
+            string 이름 = textBox6.Text;
+
+            foreach (ListViewItem 가게이름 in listView1.Items)
+            {
+
+                if (!가게이름.Text.Contains(이름))
+                {
+                    listView1.Items.Remove(가게이름);
+                    deletedItems.Add(가게이름);
+                }
+
+            }
+            string 음식종류 = textBox7.Text;
+
+            foreach (ListViewItem 종류 in listView1.Items)
+            {
+
+                if (!종류.SubItems[3].Text.Contains(음식종류))
+                {
+                    listView1.Items.Remove(종류);
+                    deletedItems.Add(종류);
+                }
+            }
+        }
+
+        private void 되돌리_Click(object sender, EventArgs e)
+        {
+            foreach (var item in deletedItems)
+            {
+                listView1.Items.Add(item); // 삭제된 항목을 다시 ListView에 추가
+            }
+            deletedItems.Clear(); // 삭제된 항목을 삭제합니다.
+        }
+    }
+    }
+
+
